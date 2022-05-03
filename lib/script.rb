1c41ede9ec56202110bce9ed67eb767b702d1c62
Alyoshka <alyoshkamyshko@gmail.com>
2022-05-02 19:59:34 -0700

    Modify ceasar cipher code to pass all tests

--- lib/script.rb
+++ lib/script.rb
@@ -1,16 +1,19 @@
 def ceasar_cipher(text, key)
     text.split("").map do |c|
         c_value = c.ord
+        # return character if not alphabetic
         if !((c_value >= 65 && c_value <= 90) || (c_value >= 97 && c_value <= 122))
-            # return character if not alphabetic
-            next c  # next returns c to map instead of to method (ceasar_cipher)
+            next c
         end
-        c_value += key
+
+        c_value += key < 26 ? key : key % 26
         if (c == c.upcase && c_value > 90) || (c == c.downcase && c_value > 122)
             c_value -= 26
+        elsif (c == c.upcase && c_value < 65) || (c == c.downcase && c_value < 97)
+            c_value += 26
         end
         c_value.chr
     end.join
 end
 
-puts ceasar_cipher("What a string!", 5)
+puts ceasar_cipher("Anyone home?", -4)
